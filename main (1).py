d03442c87c5eb8333892bf7621eed33758a67033
gnv11 <143380425+gnv11@users.noreply.github.com>
2023-08-28 11:35:54 +0530

    Add files via upload
    commit 1

---
+++ main (1).py
@@ -0,0 +1,10 @@
+print("Welcome to AskPython")
+a = 10
+b = 20
+
+def add_a_b(a, b):
+    res = a + b
+    return res
+
+result = add_a_b(a, b)  # Call the function and store the result
+print("Result:", result)  # Print the result
